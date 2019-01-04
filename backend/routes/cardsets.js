ca36bb285ce4a3713166be60e44a6d4b84ec17cf
Wilton Ngo <wiltonngo@gmail.com>
2019-01-04 16:52:43 -0500

    Now caches cardset result if expired

--- backend/routes/cardsets.js
+++ backend/routes/cardsets.js
@@ -7,17 +7,31 @@ router.get('/:id', getCardSet);
 
 async function getCardSet(req, res) {
     try {
-        const cardset = await CardSet.findOne({
+        let cardset = await CardSet.findOne({
             cardset_id: req.params.id
         });
         if (!cardset) {
-            const result = await axios.get(`https://playartifact.com/cardset/${req.params.id}/`);
+            console.log("No documents existing in DB, calling valve cardset API");
+            let result = await axios.get(`https://playartifact.com/cardset/${req.params.id}/`);
             result.data.cardset_id = req.params.id;
             cardSet = new CardSet(result.data);
             await cardSet.save();
-            return res.status(200).send(cardSet);
+        } else {
+            if (Date.now() >= new Date(cardset.expire_time * 1000)) {
+                console.log("Document expired, calling valve cardset API");
+                let result = await axios.get(`https://playartifact.com/cardset/${req.params.id}/`);
+                result.data.cardset_id = req.params.id;
+                cardSet = new CardSet(result.data);
+                await CardSet.updateOne({ cardset_id: req.params.id }, {
+                    $set: {
+                        cdn_root: cardSet.cdn_root,
+                        url: cardSet.url,
+                        expire_time: cardSet.expire_time
+                    }
+                }, { new: true});
+            }
         }
-        return res.status(200).send(cardset);
+        return res.status(200).send(cardSet);
     } catch (error) {
         console.log(error);
     }
