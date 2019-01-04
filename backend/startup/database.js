725b05615258d8d13d51423f0182bb2e93aa0c6a
Wilton Ngo <wiltonngo@gmail.com>
2019-01-04 11:48:38 -0500

    Updated deprecated connect function

--- backend/startup/database.js
+++ backend/startup/database.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 module.exports = function() {
     const db = config.get('db');
-    mongoose.connect(db)
+    mongoose.connect(db, {useNewUrlParser: true})
     .then( () => console.log(`Connected to ${db} database...`))
     .catch( (ex) => console.log(ex));
 }
