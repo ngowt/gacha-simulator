9c810534f0bcc99c3fcc0aa7b1506234a74e2ca0
Wilton Ngo <wiltonngo@gmail.com>
2019-01-05 18:44:37 -0500

    Small fixes

--- backend/services/cardsets.js
+++ backend/services/cardsets.js
@@ -1,4 +1,35 @@
 const axios = require('axios');
+const CardSet = require('../models/cardset'); 
+
+async function getCardSets() {
+    let cardSets = [];
+    const setIds = ["00", "01"];
+    for (let i = 0; i < setIds.length; i++) {
+        try {
+            let cardSet = await CardSet.findOne({ cardset_id: setIds[i] });
+            if (!cardSet) {
+                const res = await getCardSet(setIds[i]);
+                cardSet = new CardSet(res);
+                await cardSet.save();
+            }
+            if (Date.now() >= new Date(cardSet.expire_time * 1000)) {
+                const res = await getCardSet(setIds[i]);
+                cardSet = new CardSet(res);
+                await CardSet.updateOne({ cardset_id: setIds[i] }, {
+                    $set: {
+                        cdn_root: cardSet.cdn_root,
+                        url: cardSet.url,
+                        expire_time: cardSet.expire_time
+                    }
+                }, { new: true});
+            }
+            cardSets.push(cardSet);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    return cardSets;
+}
 
 async function getCardSet(id) {
     try {
@@ -10,4 +41,4 @@ async function getCardSet(id) {
     }
 }
 
-module.exports = getCardSet;
+module.exports = getCardSets;
