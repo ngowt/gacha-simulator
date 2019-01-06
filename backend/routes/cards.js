8d26e284c2a102dffb18733ddace158ca65c92dd
Wilton Ngo <wiltonngo@gmail.com>
2019-01-06 18:12:23 -0500

    Added 404 HTTP status code path to routes/cards

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -6,8 +6,8 @@ router.get('/', getCards);
 
 async function getCards(req, res) {
     const cardList = await getCardList();
+    if (!cardList) { return res.status(404).send('Unable to fetch list of cards') };
     return res.status(200).send(cardList);
 }
 
-
-module.exports = router;
+module.exports = router;   
