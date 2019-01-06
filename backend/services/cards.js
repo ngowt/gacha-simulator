94ce03491bb8e0f4af37503749445e34737cd3e3
Wilton Ngo <wiltonngo@gmail.com>
2019-01-06 14:09:23 -0500

    Code cleanup

--- backend/services/cards.js
+++ backend/services/cards.js
@@ -5,29 +5,24 @@ const CardSet = require('../models/cardset');
 async function getCardList() {
     try {
         const setIds = ["00", "01"];
-        let cardList = [];
         let result = await CardSet.findOne({ cardset_id: setIds[0] });
         if (!result || Date.now() >= new Date(result.expire_time * 1000)) {
             const pCardSet = setIds.map(async el => {
-                const res = await getCardSet(el);
-                result = new CardSet(res);    
-                return result;
+                const res = await getCardSet(el); 
+                return new CardSet(res);
             });
             const cardSet = await Promise.all(pCardSet);
             const pCardList = cardSet.map(async el => {
-                const res = await getCardsList(el.cdn_root, el.url)
-                return res; 
+                return await getCardsList(el.cdn_root, el.url)
             });
-            cardList = await Promise.all(pCardList);
+            let cardList = await Promise.all(pCardList);
             cardList = cardList.reduce((a, b) => { return a.concat(b) });
             await CardSet.remove({});
             await CardSet.insertMany(cardSet);
             await Card.remove({});
             await Card.insertMany(cardList);
-        } else {
-            cardList = await Card.find({});
         }
-        return cardList;
+        return await Card.find({});
     } catch (error) {
         console.log(error);
     }
