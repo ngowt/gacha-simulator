fd175243a620d73569f51eefb56acd74aa6c6220
Wilton Ngo <wiltonngo@gmail.com>
2019-01-06 20:14:51 -0500

    Enabled /items/, /heroes/ endpoints

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,13 +1,41 @@
 const express = require('express');
-const getCardList = require('../services/cards');
+const { getCardList, getItemList, getRandomItem, getHeroList, getRandomHero } = require('../services/cards');
 const router = express.Router();
 
-router.get('/', getCards);
+router.get('/', getAllCards);
+router.get('/items/', getAllItemCards)
+router.get('/heroes/', getAllHeroCards)
 
-async function getCards(req, res) {
-    const cardList = await getCardList();
-    if (!cardList) { return res.status(404).send('Unable to fetch list of cards') };
-    return res.status(200).send(cardList);
+async function getAllCards(req, res) {
+    const result = await getCardList();
+    if (!result) { return res.status(404).send('Unable to fetch list of cards') };
+    return res.status(200).send(result);
 }
 
-module.exports = router;   
+async function getAllHeroCards(req, res) {
+    const result = await getHeroList();
+    if (!result) { return res.status(404).send('Unable to fetch list of cards') };
+    return res.status(200).send(result);
+}
+
+async function getAllItemCards(req, res) {
+    const result = await getItemList();
+    if (!result) { return res.status(404).send('Unable to fetch list of cards') };
+    return res.status(200).send(result);
+}
+
+async function getRandomHeroCard(req, res) {
+    const result = await getRandomHero();
+    if (!result) { return res.status(404).send('Unable to fetch cards') };
+    return res.status(200).send(result);
+}
+
+async function getRandomItemCard(req, res) {
+    const result = await getRandomItem();
+    if (!result) { return res.status(404).send('Unable to fetch card') };
+    return res.status(200).send(result);
+}
+
+
+
+module.exports = router;
