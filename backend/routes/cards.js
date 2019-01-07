39737903c44ce3aa6ca4ad756a01f7cd828b5c5d
Wilton Ngo <wiltonngo@gmail.com>
2019-01-07 15:48:35 -0500

    Changed importing functions Modified route handler functions Removed getRandom() functions

--- backend/routes/cards.js
+++ backend/routes/cards.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { getCardList, getItemList, getRandomItem, getHeroList, getRandomHero } = require('../services/cards');
+const { getCardList, getItemList, getHeroList } = require('../services/cards');
 const router = express.Router();
 
 router.get('/', getAllCards);
-router.get('/items/', getAllItemCards)
-router.get('/heroes/', getAllHeroCards)
+router.get('/items/', getAllItemCards);
+router.get('/heroes/', getAllHeroCards);
 
 async function getAllCards(req, res) {
     const result = await getCardList();
@@ -23,19 +23,5 @@ async function getAllItemCards(req, res) {
     if (!result) { return res.status(404).send('Unable to fetch list of cards') };
     return res.status(200).send(result);
 }
-
-async function getRandomHeroCard(req, res) {
-    const result = await getRandomHero();
-    if (!result) { return res.status(404).send('Unable to fetch cards') };
-    return res.status(200).send(result);
-}
-
-async function getRandomItemCard(req, res) {
-    const result = await getRandomItem();
-    if (!result) { return res.status(404).send('Unable to fetch card') };
-    return res.status(200).send(result);
-}
-
-
 
 module.exports = router;
