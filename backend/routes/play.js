6f87f6cbd45c6da490c2d6e6930deaaa0695dbd6
Wilton Ngo <wiltonngo@gmail.com>
2019-01-24 17:15:32 -0500

    C:/Program Files/Git/api/play route now uses cors middleware function

--- backend/routes/play.js
+++ backend/routes/play.js
@@ -1,15 +1,21 @@
-const express = require('express');
-const CardSession = require('../models/cardSession');
-const asyncAwaitMiddleware = require('../middleware/asyncAwait');
+const express = require("express");
+const CardSession = require("../models/cardSession");
+const asyncAwaitMiddleware = require("../middleware/asyncAwait");
+const corsMiddleware = require("../middleware/cors");
 const router = express.Router();
 
-router.post('/', asyncAwaitMiddleware(startNewSession));
+router.post("/", [
+  corsMiddleware(startNewSession),
+  asyncAwaitMiddleware(startNewSession)
+]);
 
 async function startNewSession(req, res) {
-    const session = await CardSession().startOpenSession();
-    if (!session) { return res.status(404).send("Unable to retrieve cards") };
-    await session.save();
-    return res.status(200).send(session);
+  const session = await CardSession().startOpenSession();
+  if (!session) {
+    return res.status(404).send("Unable to retrieve cards");
+  }
+  await session.save();
+  return res.status(200).send(session);
 }
 
 module.exports = router;
