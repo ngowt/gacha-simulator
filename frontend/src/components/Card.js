11496973cc8b0bb79a11252bc39daff1520150fb
Wilton Ngo <wiltonngo@gmail.com>
2019-01-27 02:36:38 -0500

    Added placeholder style

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,6 +3,10 @@ import React from "react";
 export const Card = props => {
   const { card_name, large_image } = props.card;
   return (
-    <img src={large_image ? large_image.default : ""} alt={card_name.english} />
+    <img
+      className="ui placeholder"
+      src={large_image ? large_image.default : ""}
+      alt={card_name.english}
+    />
   );
 };
